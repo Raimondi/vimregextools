044c7ac23480f261cd17ce2d1008480b67a02026
Israel Chauca Fuentes <israel@chauca.net>
2012-09-19 21:01:25 -0400

    Simplify script a bit.

--- process_template.rb
+++ process_template.rb
@@ -2,17 +2,6 @@
 
 require 'erb'
 
-class Elem
-  attr_accessor :magicv, :magicm, :magicM, :magicV
-
-  def initialize(magicv, magicm, magicM, magicV)
-    @magicv = magicv
-    @magicm = magicm
-    @magicM = magicM
-    @magicV = magicV
-  end
-end
-
 def magic(magicv, magicm, magicM, magicV)
   return magicv if $magic == :very_magic
   return magicm if $magic == :magic
@@ -24,8 +13,9 @@ input = File.new(ARGV[0], "r")
 template = ERB.new(input.read)
 
 [:very_magic, :magic, :non_magic, :very_non_magic].each do | m |
+  puts 'Processing "' + m.to_s() + '"...'
   $magic = m
-  output = File.new(File.dirname(input.path) + '/' + 'parser_' + $magic.to_s + '.vimpeg' , "w")
+  output = File.new(File.join(File.dirname(input.path), 'parser_' + $magic.to_s + '.vimpeg') , "w")
   output.write(template.result())
   output.close
   `vim -c 'VimPEG!' -e -c 'q!' #{output.path}`
